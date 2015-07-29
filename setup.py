bfab15d4e42dcd6884000b313d68d21ff8692936
Sergey Tikhonov <zimbler@gmail.com>
2015-07-29 18:06:06 +0300

    version 0.1

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name='alco',
-    version='0.0.14',
+    version='0.1.0',
     packages=['alco',
               'alco.grep',
               'alco.grep.api',
