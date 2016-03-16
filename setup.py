836501ef582cce3d8f3f7334d071796f4e79a202
tumb1er <zimbler@gmail.com>
2016-03-16 13:53:57 +0300

    Bump version to 1.0.1

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ except ImportError:
 
 setup(
     name='alco',
-    version='1.0.0',
+    version='1.0.1',
     long_description=read_md('README.md'),
     packages=['alco',
               'alco.grep',
