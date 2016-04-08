56d554b12462221e5508b8264cb7b0d805283ddc
Sergey Tikhonov <zimbler@gmail.com>
2016-04-08 18:57:30 +0300

    index page

--- alco/grep/urls.py
+++ alco/grep/urls.py
@@ -5,7 +5,7 @@
 
 from django.conf.urls import url
 
-from alco.grep.views import GrepView, ShortcutView
+from alco.grep.views import GrepView, ShortcutView, IndexListView
 
 urlpatterns = [
     url(r'^(?P<name>[\w]+)/$', GrepView.as_view(), name='grep_view'),
@@ -13,4 +13,5 @@ urlpatterns = [
         ShortcutView.as_view(), name="shortcut_view"),
     url(r'^shortcut/(?P<name>[\w]+)/$',
         ShortcutView.as_view(), name="shortcut_view"),
+    url(r'^$', IndexListView.as_view(), name="index_list_view")
 ]
